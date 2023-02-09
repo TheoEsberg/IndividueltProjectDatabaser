15a4f1d3d7955ca2ed897d7965ee18a261906cf3
Theo Esberg <esbergtheo@gmail.com>
2023-02-09 10:32:26 +0100

    updates

---
+++ IndividueltProject/Controls/Students.cs
@@ -0,0 +1,23 @@
+﻿using IndividueltProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndividueltProject.Controls
+{
+    internal class Students
+    {
+        public static void ShowAllStudents()
+        {
+            var context = new IndividualProjectDbContext();
+            var students = context.Students;
+
+            foreach (var student in students.OrderBy(c => c.ClassName))
+            {
+                Console.WriteLine(student.FName + " " + student.LName + ", " + student.ClassName);
+            }
+        }
+    }
+}
---
+++ IndividueltProject/Controls/Teachers.cs
@@ -0,0 +1,30 @@
+﻿using IndividueltProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndividueltProject.Controls
+{
+    internal class Teachers
+    {
+        public static void ShowAllTeahers()
+        {
+            var context = new IndividualProjectDbContext();
+            var teachers = (from c in context.Courses join e in context.Employees on c.TeacherId equals e.EmployeeId select new
+            {
+                EmployeeID = e.EmployeeId,
+                EmployeeName = e.FullName,
+                EmployeeTitle = e.WorkTitle,
+                CourseName = c.CourseName
+            }).ToList();
+
+            foreach (var teacher in teachers.OrderBy(c => c.EmployeeTitle))
+            {
+                Console.WriteLine(teacher.CourseName + ", " + teacher.EmployeeName + ", " + teacher.EmployeeTitle);
+            }
+        }
+    }
+}
